75b0b1523f47348cd0f9376e7d2f494c7320d451
Sheng <symbi_21@hotmail.com>
2021-05-30 22:51:17 +0900

    setting remove

--- config/settings.py
+++ config/settings.py
@@ -22,8 +22,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 #GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
 #GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
-GDAL_LIBRARY_PATH="/app/.heroku/vendor/lib/libgdal.so"
-GEOS_LIBRARY_PATH="/app/.heroku/vendor/lib/libgeos_c.so"
+#GDAL_LIBRARY_PATH="/app/.heroku/vendor/lib/libgdal.so"
+#GEOS_LIBRARY_PATH="/app/.heroku/vendor/lib/libgeos_c.so"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
