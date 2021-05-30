94d330fe2343be135fdfe80d9ab3cde0241f67b3
Sheng <symbi_21@hotmail.com>
2021-05-30 22:42:06 +0900

    edit GDAL PATH

--- config/settings.py
+++ config/settings.py
@@ -20,8 +20,10 @@ env.read_env('.env')
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
+#GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
 #GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
+GDAL_LIBRARY_PATH=/app/.heroku/vendor/lib/libgdal.so
+GEOS_LIBRARY_PATH=/app/.heroku/vendor/lib/libgeos_c.so
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
