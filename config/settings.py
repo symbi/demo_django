d0efa3a6e964a3d7b45db69dc9ab10884d0faea8
Sheng <symbi_21@hotmail.com>
2021-05-30 22:36:09 +0900

    add GDAL PATH

--- config/settings.py
+++ config/settings.py
@@ -20,6 +20,8 @@ env.read_env('.env')
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
+#GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
